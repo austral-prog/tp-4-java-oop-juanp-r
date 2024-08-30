adbbbcef9206d2fd09f05f8cf9731db56b68ee2e
Juan Prieto <162722098+juanp-r@users.noreply.github.com>
2024-08-30 10:40:39 -0300

    Elevator.java

--- src/main/java/com/template/Elevator.java
+++ src/main/java/com/template/Elevator.java
@@ -1,34 +1,50 @@
 package com.template;
 
 public class Elevator {
-
+int minFloor;
+int maxFloor;
+int maxCapacity;
+int currentFloor;
+int currentPassangers;
 
     public Elevator(int minFloor, int maxFloor, int maxCapacity) {
         // Initialize elevator with minFloor, maxFloor and maxCapacity
+        this.minFloor = minFloor;
+        this.maxFloor = maxFloor;
+        this.maxCapacity = maxCapacity;
+        this.currentFloor = minFloor;
+        this.currentPassangers = 0;
     }
 
     public void moveToFloor(int floor) {
         // Move elevator to the floor if it is within the minFloor and maxFloor
+        if (floor > minFloor && floor < maxFloor) {
+            this.currentFloor = floor;
+        }
     }
 
     public void addPassenger(){
         // Add a passenger to the elevator if the elevator is not full
+        if (this.currentPassangers < this.maxCapacity){
+            this.currentPassangers++;
+        }
     }
 
     public void removePassenger(){
         // Remove a passenger from the elevator if the elevator is not empty
+        if (this.currentPassangers > 0){
+            this.currentPassangers--;
+        }
     }
 
     public int getCurrentFloor(){
         // Return the current floor of the elevator
-        return 0;
+        return this.currentFloor;
     }
 
     public int getPassengerCount(){
         // Return the number of passengers in the elevator
-        return 0;
+        return this.currentPassangers;
     }
-
-
-
 }
+
